b5fa0c0caa5906e5528787aa6e56d8bbe2e07aee
iamudyavar <aditya.udyavar@gmail.com>
2025-10-22 20:53:45 -0500

    Removed redundant DB index

--- convex/schema.js
+++ convex/schema.js
@@ -5,7 +5,6 @@ export default defineSchema({
     matches: defineTable(matchSchema)
         .index("by_vlr_id", ["vlrId"])
         .index("by_status", ["status"])
-        .index("by_time", ["time"])
         .index("by_status_time", ["status", "time"])
         .searchIndex("by_search_terms_and_status", {
             searchField: "searchTerms",
